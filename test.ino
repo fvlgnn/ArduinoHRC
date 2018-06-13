f3fd06fa869bbfbc3447ee483432f469a41e6260
Gianni F <fvlgnn@gmail.com>
2018-06-13 11:50:00 +0200

    Update test.ino

--- test.ino
+++ test.ino
@@ -1,5 +1,5 @@
 /**
- * Code controller
+ * Sketch ArduinoHRC Example
  * @author @fvlgnn <fvlgnn@gmail.com>
  * @date 1st Jul, 2014
 */
@@ -8,7 +8,7 @@
 #include <Ethernet.h>
 #include <ArduinoHRC.h>
 
-ArduinoHRC ahrc({:ARDID:}, "{:ARDKEY:}");
+ArduinoHRC ahrc("{:ARDID:}", "{:ARDKEY:}");
 
 byte mac[] = { 0xDE,0xAD,0xBE,0xEF,0xFE,0xED };
 byte ip[] = {192,168,1,234};
